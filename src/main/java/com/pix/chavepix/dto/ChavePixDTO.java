94dbc732f72bb512a302a1379ffe9b15c0e5566f
Raffaell Negreiros Guimaraes <tqi_rnegreiros@uolinc.com>
2022-01-09 02:21:58 -0300

    Correcoes

--- src/main/java/com/pix/chavepix/dto/ChavePixDTO.java
+++ src/main/java/com/pix/chavepix/dto/ChavePixDTO.java
@@ -3,22 +3,26 @@ package com.pix.chavepix.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ChavePixDTO {
 
-    private int id;
+    @ToString.Exclude
+//    private int id;
     private String nomeCLiente;
     private String chavePixCliente;
-//    private String tipoChave;
-//    private int banco;
-//    private int agencia;
-//    private int conta;
+    private String tipoChave;
+    private String banco;
+    private int agencia;
+    private int conta;
 //    private boolean statusChave;
 //    private LocalDateTime dataCriacao;
 //    private LocalDateTime dataAlteracao;
-//    private String motivoAlteracao;
+    private String motivoAlteracao;
 
 }
